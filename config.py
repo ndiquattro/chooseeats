e3981ca273dff0fc41dcd5a7da3665aa8e05a235
Nick DiQuattro <ndiquattro@gmail.com>
2015-05-30 21:52:25 +0000

    serversql

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 WTF_CSRF_ENABLED = True
-with open('codes.csv', 'rb') as csvfile:
+with open('/home/nickof4/codes.csv', 'rb') as csvfile:
     codesf = csv.DictReader(csvfile)
     for row in codesf:
         codes = row
@@ -11,9 +11,9 @@ SECRET_KEY = codes['secret2']
 # Database set-up
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+#if os.environ.get('DATABASE_URL') is None:
+#    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#else:
+SQLALCHEMY_DATABASE_URI = 'mysql://nickof4:mongotut@mysql.server/nickof4$ceatsdb'
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
