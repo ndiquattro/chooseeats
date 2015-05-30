e8ff4e26b8eab7666693057136ec46dbc5196ce3
Nick DiQuattro <ndiquattro@gmail.com>
2015-05-30 13:59:23 -0700

    switch to MySQL

--- config.py
+++ config.py
@@ -11,5 +11,9 @@ SECRET_KEY = codes['secret2']
 # Database set-up
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
